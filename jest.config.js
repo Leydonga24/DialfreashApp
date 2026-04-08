b6e5a5a8e26dddb6a37e4a58b1e8e3864d3ebb25
Karina <tu-correo@ejemplo.com>
2026-04-07 20:38:03 -0500

    fix: limpieza de node_modules y ruta de jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-native'],
 };
 
 
